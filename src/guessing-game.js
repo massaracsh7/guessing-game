39c0ec787edac858774239d1aaf8f59f203a71a6
Your Name <you@example.com>
2023-01-14 23:04:17 +0300

    feat: add gclass GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+      this.min = min;
+      this.max = max;
     }
 
     guess() {
-
+      return Math.round((this.max + this.min) / 2);
     }
 
     lower() {
-
+      return this.max = Math.round((this.max + this.min) / 2);
     }
 
     greater() {
-
+      return this.min = Math.round((this.max + this.min) / 2);
     }
 }
 
